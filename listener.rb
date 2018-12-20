6d8ff0679aaa7381ea7c95959391e597745c356b
Ricardo Justo <ricardo@debian.lan>
2018-12-20 23:14:37 +0000

    add http service to relay payload

--- listener.rb
+++ listener.rb
@@ -1,6 +1,44 @@
 # frozen_string_literal: true
 
 require 'pg'
+require 'json'
+require 'uri'
+require 'net/http'
+require 'pry'
+
+class Webhook
+  class << self
+    def send_notification(payload)
+      uri = URI.parse(database_conn(payload))
+      request = request_settings(uri)
+      http_send(uri, request)
+    end
+
+    private
+
+    def database_conn(notification)
+      "#{ENV['WEBHOOK_URL']}?notification=#{notification}"
+    end
+
+    def http_send(uri, request)
+      response = Net::HTTP.start(uri.hostname, uri.port, request_options(uri)) do |http|
+        http.request(request)
+      end
+      response
+    end
+
+    def request_settings(uri)
+      request = Net::HTTP::Post.new(uri)
+      request
+    end
+
+    def request_options(uri)
+      {
+        use_ssl: uri.scheme == 'https'
+      }
+    end
+  end
+end
 
 class Listener
   def initialize
@@ -8,36 +46,53 @@ class Listener
   end
 
   def listen
-    @conn.wait_for_notify do |_event, _pid, payload|
-      puts payload
+    channels.each { |channel| @conn.exec("LISTEN #{channel}") }
+    loop do
+      @conn.wait_for_notify do |channel, pid, payload|
+        puts "Received a NOTIFY on channel #{channel}"
+        puts "from PG backend #{pid}"
+        puts "saying #{JSON.parse(payload).reject { |k,_| k == 'data' }}"
+        Webhook.send_notification(payload)
+      end
     end
+  ensure
+    channels.each { |channel| @conn.exec("UNLISTEN #{channel}") }
+    puts 'unlistened'
   end
 
   def create
     create_function
-    create_trigger
-    puts 'function and trigger have been created'
-  rescue => error
+    create_triggers
+    puts 'function and triggers have been created'
+  rescue StandardError => error
     puts error
   end
 
   def drop
-    drop_trigger
+    drop_triggers
     drop_function
-    puts 'function and trigger have been deleted'
-  rescue => error
+    puts 'function and triggers have been deleted'
+  rescue StandardError => error
     puts error
   end
 
   private
 
+  def channels
+    ['events']
+  end
+
+  def tables
+    ['orders']
+  end
+
   def create_connection
     PG::Connection.open(
-      host: 'localhost',
-      dbname: '',
-      user: '',
-      password: '',
-      port: 5432,
+      host: ENV['PG_HOST'],
+      dbname: ENV['PG_DBNAME'],
+      user: ENV['PG_USER'],
+      password: ENV['PG_PASSWORD'],
+      port: ENV['PG_PORT']
     )
   end
 
@@ -68,16 +123,24 @@ class Listener
     @conn.exec('DROP FUNCTION notify_event();')
   end
 
-  def create_trigger
-    trigger = <<-SQL
-      CREATE TRIGGER notify_order_event
-      AFTER INSERT OR UPDATE OR DELETE ON orders
-        FOR EACH ROW EXECUTE PROCEDURE notify_event();
-    SQL
-    @conn.exec(trigger)
+  def create_triggers
+    tables.each do |table|
+      trigger = <<-SQL
+        CREATE TRIGGER notify_#{table}_events
+        AFTER INSERT OR UPDATE OR DELETE ON #{table}
+          FOR EACH ROW EXECUTE PROCEDURE notify_event();
+      SQL
+      @conn.exec(trigger)
+    end
   end
 
-  def drop_trigger
-    @conn.exec('DROP TRIGGER notify_order_event ON orders')
+  def drop_triggers
+    tables.each do |table|
+      @conn.exec("DROP TRIGGER notify_#{table}_events ON #{table}")
+    end
   end
 end
+listener = Listener.new
+listener.drop
+listener.create
+listener.listen
